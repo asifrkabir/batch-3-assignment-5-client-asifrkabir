70506e0715685aaa277293831a886c7799e419d7
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-08-30 14:37:45 +0600

    feat: RouteChangeScrollToTop implementation

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,30 +3,34 @@ import { Layout } from "antd";
 import Navbar from "./Navbar";
 import AppFooter from "./AppFooter";
 import { Outlet } from "react-router-dom";
+import RouteChangeScrollToTop from "../ui/RouteChangeScrollToTop";
 
 const { Content } = Layout;
 
 const MainLayout: React.FC = () => {
   return (
-    <Layout style={{ background: "#fff" }}>
-      <Navbar />
+    <>
+      <RouteChangeScrollToTop />
+      <Layout style={{ background: "#fff" }}>
+        <Navbar />
 
-      <Content style={{ padding: "24px" }}>
-        <div
-          style={{
-            padding: 24,
-            minHeight: "100vh",
-            background: "#fff",
-            borderRadius: "5px",
-            maxWidth: "1400px",
-            margin: "0 auto",
-          }}>
-          <Outlet />
-        </div>
-      </Content>
+        <Content style={{ padding: "24px" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: "100vh",
+              background: "#fff",
+              borderRadius: "5px",
+              maxWidth: "1400px",
+              margin: "0 auto",
+            }}>
+            <Outlet />
+          </div>
+        </Content>
 
-      <AppFooter />
-    </Layout>
+        <AppFooter />
+      </Layout>
+    </>
   );
 };
 
