191cedb7dd0d43ab86decb9ba9117db9bc8ff98c
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-08-30 17:08:00 +0600

    feat: Refresh Token implementation

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,19 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
+import { toast } from "sonner";
+import { logout, setUser } from "../features/auth/authSlice";
+import { TError } from "../../types";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: import.meta.env.VITE_BASE_API_URL,
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).persisted.auth.token;
@@ -14,9 +25,50 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  if (result?.error?.status === 404) {
+    toast.error((result?.error as TError)?.data?.message);
+  }
+
+  if (result?.error?.status === 401) {
+    const res = await fetch(
+      `${import.meta.env.VITE_BASE_API_URL}/auth/refresh-token`,
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+
+    const data = await res.json();
+
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).persisted.auth.user;
+
+      api.dispatch(
+        setUser({
+          user,
+          token: data.data.accessToken,
+        })
+      );
+
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logout());
+    }
+  }
+
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery,
-  tagTypes: ["bikes", "rentals","profile","users"],
+  baseQuery: baseQueryWithRefreshToken,
+  tagTypes: ["bikes", "rentals", "profile", "users"],
   endpoints: () => ({}),
 });
